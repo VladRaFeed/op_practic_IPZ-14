ba0cd9a21bc7d41714004005092d0f09bbbb5106
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2023-09-06 09:08:34 +0000

    Deploying to gh-pages from @ VladRaFeed/op_practic_IPZ-14@0cd8664cc38d3bac09a6165d31d518fafedc60f3 🚀

---
+++ main.219bb5c3.js
@@ -0,0 +1,2 @@
+const o=[7,34,11,5,52,29,3];!function(o){const t=o.length;let l=!0;for(;l;){l=!1;for(let n=0;n<t-1;n+=1)if(o[n]>o[n+1]){let t=o[n];o[n]=o[n+1],o[n+1]=t,l=!0}}}(o),console.log(o);
+//# sourceMappingURL=main.219bb5c3.js.map
---
+++ main.44bcdf36.js
@@ -0,0 +1,2 @@
+!function(){var o=[7,34,11,5,52,29,3];!function(o){for(var n=o.length,r=!0;r;){r=!1;for(var f=0;f<n-1;f+=1)if(o[f]>o[f+1]){var a=o[f];o[f]=o[f+1],o[f+1]=a,r=!0}}}(o),console.log(o)}();
+//# sourceMappingURL=main.44bcdf36.js.map
